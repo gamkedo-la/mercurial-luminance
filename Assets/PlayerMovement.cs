16f16db162e15ad1dd64cbe275323c5c5d9dd452
Justin Chin <jchingames@gmail.com>
2020-02-12 20:05:01 -0800

    Movement Forward and Backwards, left and right with Particle body and particle trail

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    
+    private float turnSpeed = 0.0f;
+    private float driveSpeed = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void FixedUpdate()
+    {
+        float kValue = 0.3f;
+        turnSpeed = Input.GetAxisRaw("Horizontal") * (1.0f - kValue) + turnSpeed * kValue;
 
+        kValue = 0.9f;
+        driveSpeed = Input.GetAxisRaw("Vertical") * (1.0f - kValue) + driveSpeed * kValue;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * 5.0f;
+        transform.position += transform.forward * Time.deltaTime * 5.0f * driveSpeed;
+
+        //spin with our keys
+        transform.Rotate(Vector3.up, turnSpeed * 40.0f * Time.deltaTime);
     }
 }
