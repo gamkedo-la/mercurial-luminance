f4c7fbe8a64d5073e371b18891f212d45b4158c8
Justin Chin <jchingames@gmail.com>
2020-02-29 23:46:02 -0800

    Added in ground hover (Forgot to push!) and added the script to the other glows for testing purposes.  Added in little squished sphere again for testing purposes.

---
+++ Assets/Scripts/GroundHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit rhInfo;
+        float minAlt = 1.0f;
+        if (Physics.Raycast(transform.position, Vector3.down, out rhInfo, minAlt))
+        {
+            transform.position = rhInfo.point + minAlt * Vector3.up;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private float turnSpeed = 1.0f;
     private float driveSpeed = 0.0f;
+    private float pitchFacing = 0.0f;
+    private float yawFacing = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void FixedUpdate()
@@ -28,6 +30,18 @@ public class PlayerMovement : MonoBehaviour
         transform.position += transform.forward * Time.deltaTime * 5.0f * driveSpeed;
 
         //spin with our keys
-        transform.Rotate(Vector3.up, turnSpeed * 40.0f * Time.deltaTime);
+        //transform.Rotate(Vector3.up, turnSpeed * 40.0f * Time.deltaTime);
+        //Mouse
+        //transform.Rotate(Vector3.up, Input.GetAxisRaw("Mouse X") * 40.0f * Time.deltaTime);
+        //transform.Rotate(transform.right, Input.GetAxisRaw("Mouse Y") * 40.0f * Time.deltaTime);
+
+        yawFacing += turnSpeed * 40.0f * Time.deltaTime;
+        yawFacing += Input.GetAxisRaw("Mouse X") * 40.0f * Time.deltaTime;
+        
+        pitchFacing -= Input.GetAxisRaw("Mouse Y") * 40.0f * Time.deltaTime;
+        pitchFacing = Mathf.Clamp(pitchFacing, -30.0f, 30.0f);
+
+        transform.rotation = Quaternion.AngleAxis(yawFacing, Vector3.up) *
+            Quaternion.AngleAxis(pitchFacing, Vector3.right);
     }
 }
