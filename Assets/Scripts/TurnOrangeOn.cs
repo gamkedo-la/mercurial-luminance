b5dceb88e146ad4453d15340803afa67af05d140
Justin Chin <jchingames@gmail.com>
2020-03-22 12:43:23 -0700

    Added in Types of Sprites and testing Emotional Sprites "Fear" Sprite.

--- Assets/Scripts/TurnOrangeOn.cs
+++ Assets/Scripts/TurnOrangeOn.cs
@@ -8,12 +8,33 @@ public class TurnOrangeOn : MonoBehaviour
     public GameObject orangeSpirit;
     public GameObject greenParticle;
     public GameObject greenSpirit;
+    public GameObject redParticle;
+    public GameObject redSpirit;
+    public GameObject yellowParticle;
+    public GameObject yellowSpirit;
+    public GameObject blueParticle;
+    public GameObject blueSpirit;
+    public GameObject purpleParticle;
+    public GameObject purpleSpirit;
+
     void Start()
     {
         orangeSpirit = GameObject.FindGameObjectWithTag("OrangeSpirit");
         orangeParticle = GameObject.Find("/Player/TPOrange");
         greenSpirit = GameObject.FindGameObjectWithTag("GreenSpirit");
         greenParticle = GameObject.Find("/Player/TPGreen");
+        redSpirit = GameObject.FindGameObjectWithTag("RedSpirit");
+        redParticle = GameObject.Find("/Player/TPRed");        
+        yellowSpirit = GameObject.FindGameObjectWithTag("YellowSpirit");
+        yellowParticle = GameObject.Find("/Player/TPYellow");
+        blueSpirit = GameObject.FindGameObjectWithTag("BlueSpirit");
+        blueParticle = GameObject.Find("/Player/TPBlue");
+        purpleSpirit = GameObject.FindGameObjectWithTag("PurpleSpirit");
+        purpleParticle = GameObject.Find("/Player/TPPurple");
+        purpleParticle.SetActive(false);
+        blueParticle.SetActive(false);
+        yellowParticle.SetActive(false);
+        redParticle.SetActive(false);
         orangeParticle.SetActive(false);
         greenParticle.SetActive(false);
     }
@@ -28,6 +49,26 @@ public class TurnOrangeOn : MonoBehaviour
         if(other.tag == "GreenSpirit")
         {
             greenParticle.SetActive(true);
+        }
+
+        if (other.tag == "RedSpirit")
+        {
+            redParticle.SetActive(true);
+        }
+
+        if (other.tag == "YellowSpirit")
+        {
+            yellowParticle.SetActive(true);
+        }
+
+        if (other.tag == "BlueSpirit")
+        {
+            blueParticle.SetActive(true);
+        }
+
+        if (other.tag == "PurpleSpirit")
+        {
+            purpleParticle.SetActive(true);
         }
     }
 }
