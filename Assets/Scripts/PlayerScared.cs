7a165a430f542b38882c9d1514dced51cc863fa1
Justin Chin <jchingames@gmail.com>
2020-03-29 12:10:40 -0700

    Adjusted following spirits to slow when close and to catch up when far.

---
+++ Assets/Scripts/PlayerScared.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScared : MonoBehaviour
+{
+    public GameObject playerOk;
+    public GameObject playerTrailParticle;
+    public GameObject playerScaredMoment;
+    public GameObject playerScared;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOk = GameObject.FindGameObjectWithTag("PlayerOk");
+        playerTrailParticle = GameObject.FindGameObjectWithTag("PlayerTrailParticle");
+        playerScaredMoment = GameObject.FindGameObjectWithTag("ScaredMoment");        
+        playerScared = GameObject.FindGameObjectWithTag("Scared");
+        playerOk.SetActive(true);
+        playerTrailParticle.SetActive(true);
+        playerScaredMoment.SetActive(false);
+        playerScared.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "fearMonster")
+        {
+            transform.position = Random.insideUnitCircle * 0.5f;
+            transform.position = Random.insideUnitCircle * 0.5f;
+            transform.position = Random.insideUnitCircle * 0.5f;
+            playerOk.SetActive(false);
+            playerTrailParticle.SetActive(false);
+            playerScared.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/VectorFollow.cs
+++ Assets/Scripts/VectorFollow.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class VectorFollow : MonoBehaviour
 {
     public GameObject player;
+    public bool Activated;
 
     float mSpeed = 5.0f;
+    float cSpeed = 1.0f;
+    float sSpeed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        Activated = false;
     }
 
     // Update is called once per frame
@@ -25,11 +29,31 @@ public class VectorFollow : MonoBehaviour
         Vector3 mLookDirection = (player.transform.position - transform.position);
         float dist = (player.transform.position - transform.position).magnitude;
         //print("Distance to other: " + dist);
-        if (dist < 5.0f)
+        if (dist < 2.0f && Activated == false)
+            {
+                Activated = true;
+                transform.Translate(0.0f, 0.0f, cSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
+            }
+        else if (dist < 1.0f && Activated)
         {
-            transform.Translate(0.0f, 0.0f, mSpeed * Time.deltaTime);   
+            transform.Translate(0.0f, 0.0f, sSpeed * Time.deltaTime);
             //transform.LookAt(player.transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
+        }
+
+        else if (dist > 2.0f && Activated)
+        {
+            transform.Translate(0.0f, 0.0f, mSpeed * Time.deltaTime);
+            //transform.LookAt(player.transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
+        }
+
+        else if (dist < 2.0f && Activated)
+        {
+            transform.Translate(0.0f, 0.0f, cSpeed * Time.deltaTime);   
+            //transform.LookAt(player.transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);           
         }
 
 
