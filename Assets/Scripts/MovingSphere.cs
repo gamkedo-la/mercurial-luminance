78309b48028194a1d1f8af58e7a373a2a39d6b3e
Justin Chin <jchingames@gmail.com>
2020-03-29 14:50:06 -0700

    Partial Inprogress update of tunning velocity and acceleration on the X & Z Axis 2D Plane

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    Vector3 velocity;
+    Vector3 acceleration;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +25,12 @@ public class MovingSphere : MonoBehaviour
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        transform.localPosition = new Vector3(playerInput.x, 0f, playerInput.y);
+
+        Vector3 desiredVelocity = 
+            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        Vector3 displacement = velocity * Time.deltaTime;
+
+        transform.localPosition += displacement;
     }
 }
