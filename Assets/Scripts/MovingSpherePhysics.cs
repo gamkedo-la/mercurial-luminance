44bea24ab595f5d4a1519cbaca646ce6428e7802
Justin Chin <jchingames@gmail.com>
2020-03-29 18:59:43 -0700

    Got physics working so I can actually move, saved old movement on other script, stopped myself from falling through the ground, removed delay on the fear emotion so it happens instantiously

---
+++ Assets/Scripts/MovingSpherePhysics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpherePhysics : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    Vector3 velocity;
+    Vector3 acceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+
+        Vector3 desiredVelocity =
+            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x =
+            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z =
+            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 displacement = velocity * Time.deltaTime;
+
+        Vector3 newPosition = transform.localPosition + displacement;
+       
+        transform.localPosition = newPosition;
+    }
+}
