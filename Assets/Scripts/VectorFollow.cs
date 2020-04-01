428dbf18b61f503ad779de73fc756482462264e4
Justin Chin <jchingames@gmail.com>
2020-03-31 20:46:51 -0700

    Cleaned up Code readability and simplified code refactoring

--- Assets/Scripts/VectorFollow.cs
+++ Assets/Scripts/VectorFollow.cs
@@ -7,9 +7,12 @@ public class VectorFollow : MonoBehaviour
     public GameObject player;
     public bool Activated;
 
-    float mSpeed = 5.0f;
-    float cSpeed = 1.0f;
-    float sSpeed = 0.5f;
+    float fastSpeed = 5.0f;
+    float mediumSpeed = 1.0f;
+    float slowSpeed = 0.5f;
+    float ai_ActivateRange = 2.0f;
+    float fastRange = 2.0f;
+    float slowRange = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,33 +32,32 @@ public class VectorFollow : MonoBehaviour
         Vector3 mLookDirection = (player.transform.position - transform.position);
         float dist = (player.transform.position - transform.position).magnitude;
         //print("Distance to other: " + dist);
-        if (dist < 2.0f && Activated == false)
+        if(Activated == false)
+        {
+            if (dist < ai_ActivateRange)
             {
                 Activated = true;
-                transform.Translate(0.0f, 0.0f, cSpeed * Time.deltaTime);
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
             }
-        else if (dist < 1.0f && Activated)
-        {
-            transform.Translate(0.0f, 0.0f, sSpeed * Time.deltaTime);
-            //transform.LookAt(player.transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
         }
-
-        else if (dist > 2.0f && Activated)
+        else
         {
-            transform.Translate(0.0f, 0.0f, mSpeed * Time.deltaTime);
-            //transform.LookAt(player.transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);
-        }
-
-        else if (dist < 2.0f && Activated)
-        {
-            transform.Translate(0.0f, 0.0f, cSpeed * Time.deltaTime);   
-            //transform.LookAt(player.transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), 0.1f);           
-        }
-
-
+            float turnRate = 0.1f;
+            float speedNow;
+            if (dist < slowRange)
+            {
+                speedNow = slowSpeed;
+                turnRate = 0.05f;
+            }
+            else if (dist > fastRange)
+            {
+                speedNow = fastSpeed;
+            }
+            else
+            {
+                speedNow = mediumSpeed;   
+            }
+            transform.Translate(0.0f, 0.0f, speedNow * Time.deltaTime);   
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), turnRate);
+        }   
     }
 }
