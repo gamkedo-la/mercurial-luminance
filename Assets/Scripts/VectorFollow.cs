7f9f4fd3f8808380b754d8666c4abb59015dc872
Justin Chin <jchingames@gmail.com>
2020-03-31 20:56:58 -0700

    Spirits now orbit at random speeds

--- Assets/Scripts/VectorFollow.cs
+++ Assets/Scripts/VectorFollow.cs
@@ -9,16 +9,19 @@ public class VectorFollow : MonoBehaviour
 
     float fastSpeed = 5.0f;
     float mediumSpeed = 1.0f;
-    float slowSpeed = 0.5f;
+    float orbitSpeed;
     float ai_ActivateRange = 2.0f;
-    float fastRange = 2.0f;
-    float slowRange = 1.0f;
+    float fastRange;
+    float orbitRange;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Activated = false;
+        orbitRange = Random.Range(0.8f, 3.0f);
+        fastRange = orbitRange + Random.Range(1.0f, 2.5f);
+        orbitSpeed = Random.Range(0.4f, 1.4f);
     }
 
     // Update is called once per frame
@@ -43,10 +46,13 @@ public class VectorFollow : MonoBehaviour
         {
             float turnRate = 0.1f;
             float speedNow;
-            if (dist < slowRange)
+            float angleOffset = 0.0f;
+
+            if (dist < orbitRange)
             {
-                speedNow = slowSpeed;
+                speedNow = orbitSpeed;
                 turnRate = 0.05f;
+                angleOffset = 90.0f;
             }
             else if (dist > fastRange)
             {
@@ -57,7 +63,9 @@ public class VectorFollow : MonoBehaviour
                 speedNow = mediumSpeed;   
             }
             transform.Translate(0.0f, 0.0f, speedNow * Time.deltaTime);   
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(mLookDirection), turnRate);
+            transform.rotation = Quaternion.Slerp(transform.rotation, 
+                Quaternion.LookRotation(mLookDirection) * Quaternion.AngleAxis(angleOffset, Vector3.up), 
+                turnRate);
         }   
     }
 }
