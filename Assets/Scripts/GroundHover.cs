8e5aadd0e9ac73922ab0fbf38589810a560a49f8
Justin Chin <jchingames@gmail.com>
2020-03-31 20:28:38 -0700

    spirit's don't stack anymore

--- Assets/Scripts/GroundHover.cs
+++ Assets/Scripts/GroundHover.cs
@@ -16,7 +16,8 @@ public class GroundHover : MonoBehaviour
     void Update()
     {
         RaycastHit rhInfo;
-        if (Physics.Raycast(transform.position, Vector3.down, out rhInfo, minAlt))
+        LayerMask cameraMask = ~LayerMask.GetMask("Player", "NPC"); // ~ for "everything but"
+        if (Physics.Raycast(transform.position, Vector3.down, out rhInfo, minAlt, cameraMask))
         {
             transform.position = rhInfo.point + minAlt * Vector3.up;
         }
