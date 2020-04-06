102afbb91ce2333196502a4ac54d45f040db22f4
powerproust <clement.bachellerie@gmail.com>
2020-04-05 20:36:34 -0400

    Smoother camera
    Smoother camera translations & "travel" mode: wider angle (like flowers)

--- Assets/Scripts/camerachase.cs
+++ Assets/Scripts/camerachase.cs
@@ -4,18 +4,81 @@ using UnityEngine;
 
 public class camerachase : MonoBehaviour
 {
+    private Vector3 velocity = Vector3.zero;
+    public Rigidbody rb;
+    public float timer;
+    public bool SwitchFieldOfViewInterp;
+    public bool FirstInitSwitch;
+    public float timerFieldOfView;
+    public float durationFieldOfView;
+    public float StartFieldOfView;
+    public float EndFieldOfView;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        timer = 1.5f;
+        durationFieldOfView = 0.8f;
+        SwitchFieldOfViewInterp = false;
+        timerFieldOfView = durationFieldOfView;
+        StartFieldOfView = 90.0F;
+        EndFieldOfView = 60.0F;
+        FirstInitSwitch = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 camOffSet = -transform.forward * 5.0f;
+        float Offsetx;
+        float Offsety;
+        float FieldOfView;
+        float SpeedTranslate;
+        
+        //If the player keeps a certain speed, we enter "travel mode" camera: larger view, bit like flowers
+        if(rb.velocity.magnitude >= 9.0)
+        {
+            timer -= Time.deltaTime;
+        } else
+        {
+            timer = 1.5f;
+        }
+
+        if (timer <= 0)
+        {
+            Offsetx = 5.0f;
+            Offsety = 0.3f;
+            FieldOfView = 90.0F;
+
+            SpeedTranslate = 0.7F;
+
+            if (!SwitchFieldOfViewInterp)
+            {
+                SwitchFieldOfViewInterp = true;
+                timerFieldOfView = 0.0F;
+                StartFieldOfView = 60.0F;
+                EndFieldOfView = 90.0F;
+                FirstInitSwitch = true;
+            }
+        }
+        else
+        {
+            Offsetx = 5.0f;
+            Offsety = 3.0f;
+            FieldOfView = 60.0F;
+            SpeedTranslate = 0.3F;
+
+            if (SwitchFieldOfViewInterp && FirstInitSwitch)
+            {
+                timerFieldOfView = 0.0F;
+                SwitchFieldOfViewInterp = false;
+                StartFieldOfView = 90.0F;
+                EndFieldOfView = 60.0F;
+            }
+        }
+        Vector3 camOffSet = -transform.forward * Offsetx;
         camOffSet.y = 0.0f;
-        camOffSet = camOffSet.normalized * 5.0f;
+        camOffSet = camOffSet.normalized * Offsety;
         camOffSet += Vector3.up * 3.0f;
 
         RaycastHit rhInfo;
@@ -29,11 +92,17 @@ public class camerachase : MonoBehaviour
             scaledBackBy = camOffSet.magnitude / camOffsetLength;
         }
 
-        Camera.main.transform.position = transform.position + camOffSet;
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, transform.position + camOffSet, ref velocity, SpeedTranslate);
         Camera.main.transform.LookAt(transform.position + Vector3.up * 2.0f * scaledBackBy);
 
-
-        
-        
+        if (Camera.main.fieldOfView != EndFieldOfView)
+        {
+            timerFieldOfView += Time.deltaTime;
+            if (timerFieldOfView > durationFieldOfView)
+            {
+                timerFieldOfView = durationFieldOfView;
+            }
+            Camera.main.fieldOfView = Mathf.Lerp(StartFieldOfView, EndFieldOfView, timerFieldOfView / durationFieldOfView);
+        }
     }
 }
