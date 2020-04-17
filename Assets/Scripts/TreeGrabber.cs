9a871c3f0b649bd70020f224a840d3f4a2e3048d
Justin Chin <jchingames@gmail.com>
2020-04-16 18:43:47 -0700

    Beginning of Zone 1 Triggers and progression setup.  Creating Ramp and Tree triggers along with setting up the forest

---
+++ Assets/Scripts/TreeGrabber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrabber : MonoBehaviour
+{
+    public GameObject player;
+    VectorFollow Activated;
+    public GameObject Orange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Orange = GameObject.FindGameObjectWithTag("OrangeSpirit");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
