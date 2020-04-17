60213392234e248f3b86605ea5e5f4b74b7fdb85
Justin Chin <jchingames@gmail.com>
2020-04-16 19:36:49 -0700

    Created repeatable script for tree/sprite activation, and moving in new forest treetop blender model

---
+++ Assets/TreeTopActivator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTopActivator : MonoBehaviour
+{
+    public GameObject[] ListToEnable;
+    public GameObject glowToMatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < ListToEnable.Length; i++)
+        {
+            ListToEnable[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (transform.position - glowToMatch.transform.position).magnitude;
+        if (dist < 10)
+        {
+            for (int i = 0; i < ListToEnable.Length; i++)
+            {
+                ListToEnable[i].SetActive(true);
+            }
+            Debug.Log("Triggered by distance");
+            this.enabled = false;
+        }
+    }
+}
