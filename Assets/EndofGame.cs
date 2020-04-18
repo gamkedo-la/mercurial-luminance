3f3ac446aa0b77b78ca2fd5ca3e91f59f9e6c563
Justin Chin <jchingames@gmail.com>
2020-04-17 17:42:46 -0700

    Beginning of creating end game canvas

---
+++ Assets/EndofGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndofGame : MonoBehaviour
+{
+    PlayerMovement cursorunlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
