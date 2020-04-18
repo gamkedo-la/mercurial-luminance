ab9cc1ba113e446e30163ce11b16b1242557d7e1
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-04-18 19:53:46 +0200

    Trigger "reaching the gate" music when close to the gate.

---
+++ Assets/Scripts/Audio/PlayAudioOnCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnCollision : MonoBehaviour
+{
+    public bool played = false;
+    private AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(!played && collider.gameObject.tag == "Player")
+        {
+            played = true;
+            audio.Play();
+        }
+    }
+
+}
